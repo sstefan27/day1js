3c4e7ba8a936f828cc9a351a2bef577d38b14ab4
sstefan27 <stefanbaiduc@gmail.com>
2021-07-12 16:04:26 +0300

    commit

---
+++ myscripy.js
@@ -0,0 +1,58 @@
+function loadData(){
+    const url = 'https://randomuser.me/api/?results=5';
+    const ul = document.getElementById("authors");
+    const container = document.getElementById("authorsContainer");
+    ul.innerHTML= "";
+
+    function createNode(element) {
+        return document.createElement(element);
+    }
+    
+    function append(parent, el) {
+        return parent.appendChild(el);
+    }
+
+    fetch(url).
+        then(resp => resp.json()).
+        then(data => {
+            let li = createNode('li');
+            let span = createNode('span');
+            span.innerHTML = JSON.stringify(data, null, ' ');
+            append(li, span);
+            append(ul, li);
+
+            var button = document.getElementById("makePretty");
+            if (button) {
+                container.removeChild(button);
+            }
+
+            button = createNode('button');
+            button.id="makePretty";
+            button.innerHTML = "Make pretty";
+            append(container, button);
+
+            button.addEventListener('click', () => {
+                ul.innerHTML = "";
+                let authorsData = data.results
+                authorsData.map(function(author) {
+                    let li = createNode('li');
+                    let img = createNode('img');
+                    let span = createNode('span');
+                    img.src = author.picture.medium;
+                    span.innerHTML = `${author.name.first} ${author.name.last} ${author.gender} ${author.nat} ${author.location.city}`;
+                    var buttonExp = createNode('button');
+                    buttonExp.innerHTML = 'details';
+                    buttonExp.addEventListener('click', () => {
+                        window.open("2nd.html")
+                    })
+                    append(li, img);
+                    append(li, span);
+                    append(li,buttonExp);
+                    append(ul, li);
+                });
+            });
+        }).catch(function(error){
+            console.log(">> "+ error);
+        });
+}
+
